04a3fb369d91f22ccafbf7ba030c59f21975de93
Sumip Chaudhary <sumip780@gmail.com>
2024-07-16 07:33:10 +0545

    Updated activity controller

--- modules/activity/activity.controller.js
+++ modules/activity/activity.controller.js
@@ -60,17 +60,18 @@ const updateById = async (id, payload) => {
         { isCompleted: true }
       );
     } else {
-      // Ensure the activity is not marked as completed if any subactivity is incomplete
-      if (activity.subactivities && activity.subactivities.length > 0) {
-        const incompleteSubactivities = activity.subactivities.filter(
-          (sub) => !sub.isCompleted
-        );
-        if (incompleteSubactivities.length > 0) {
-          throw new Error(
-            "Cannot mark activity as completed. Some subactivities are not completed."
-          );
-        }
-      }
+      // // Ensure the activity is not marked as completed if any subactivity is incomplete
+      // if (activity.subactivities && activity.subactivities.length > 0) {
+      //   const incompleteSubactivities = activity.subactivities.filter(
+      //     (sub) => !sub.isCompleted
+      //   );
+      //   if (incompleteSubactivities.length > 0) {
+      //     throw new Error(
+      //       "Cannot mark activity as completed. Some subactivities are not completed."
+      //     );
+      //   }
+      // }
+      console.log("the activity is updated as not completed");
     }
 
     // Update the activity
